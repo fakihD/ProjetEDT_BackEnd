f69f9d7b74cefec87916b24b0bf226fed504bf46
FakihD <fakih.darkaoui@hotmail.com>
2019-01-26 15:52:18 +0100

    Gestion des professeurs

--- routes/ProfRoute.js
+++ routes/ProfRoute.js
@@ -1,64 +1,83 @@
-// ---- EXPRESS JS - Framework
-
-let express = require('express'),
-    app = express();
+express = require('express'),
+app = express();
+session = require('cookie-session');
 
 // --- middleware
 // - body-parser needed to catch and to treat information inside req.body
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(session({secret: 'todotopsecret'}))
+
 // -- Load model needed for the project
-require('./Burger');
+require('../models/Prof');
+
+lienErreur = '/error';
+lienFindAll = '/AllProfs';
+lienAjouter = '/AjouterProf';
+lienModifier = '/ModifierProf';
+lienSupprimer = '/SupprimerProf/:id';
+lienGet = '/GetProf/:id';
+
+pageErreur ='';
+pageProfs = '';
+pageProf = '';
+
+// -- ERROR
+app.get(lienErreur, function(req, res) {
+    res.render(pageErreur);
+})
 
 // -- FIND ALL
-app.get('/burgers', function (req, res) {
-    let Burger = mongoose.model('Burger');
-    Burger.find().then((burgers)=>{
-        res.status(200).json(burgers)
+app.get(lienFindAll, function (req, res) {
+    let Prof = mongoose.model('Prof');
+    Prof.find().then((profs)=>{
+        res.render(pageProfs, profs);
     })
 });
 // -- CREATE
-app.post('/burgers', function (req, res) {
-    let Burger = mongoose.model('Burger');
-    let newBurger = new Burger(req.body);
-    newBurger.id = newBurger._id;
+app.post(lienAjouter, function (req, res) {
+    let Prof = mongoose.model('Prof');
+    let newProf = new Prof(req.body);
+    newProf.id = newProf._id;
 
-    newBurger.save().then(()=>{
-        res.status(200).json(newBurger)
+    newProf.save().then(()=>{
+        res.redirect(lienFindAll);
     },(err)=>{
-        res.status(400).json(err);
+        res.redirect(lienErreur);
     })
 });
 
-// -- DELETE
-app.delete('/burgers/:id', function (req, res) {
-    let Burger = mongoose.model('Burger');
-    Burger.find({id : req.params.id}).deleteOne().then(()=>{
-        res.status(204).json()
-    },(err)=>{
-        res.status(400).json(err);
-    });
-});
 // -- UPDATE
-app.put('/burgers/:id', function (req, res) {
-    mongoose.model('Burger').updateOne({id : req.params.id}, {$set : req.body}, (err, updatedBurger)=>{
+app.put(lienModifier, function (req, res) {
+    mongoose.model('Prof').updateOne({id : req.body.id}, {$set : req.body}, (err, updatedProf)=>{
        if(err){
-           res.status(400).json(err);
+            res.redirect(lienErreur);
        }else{
-           res.status(200).json(updatedBurger)
+            res.redirect(lienFindAll);
        }
     });
 });
+
+// -- DELETE
+app.delete(lienSupprimer, function (req, res) {
+    let Prof = mongoose.model('Prof');
+    Prof.find({id : req.params.id}).deleteOne().then(()=>{
+        res.redirect(lienFindAll);
+    },(err)=>{
+        res.redirect(lienErreur);
+    });
+});
+
 // -- READ
-app.get('/burgers/:id', function (req, res) {
-    mongoose.model('Burger').findOne({id : req.params.id}).then((burger)=>{
-        if(burger){
-            res.status(200).json(burger)
+app.get(lienGet, function (req, res) {
+    mongoose.model('Prof').findOne({id : req.params.id}).then((prof)=>{
+        if(prof){
+            res.render(pageProf, prof);
         }else{
-            res.status(404).json({message : "Not Found"})
+            res.status(404).json({message : "Inexistant"});
         }
     },(err)=>{
-        res.status(400).json(err)
+        res.redirect(lienErreur);
     });
 });
